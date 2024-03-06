bab24340af2acff76d168e1ce492752c0071d34f
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-03-06 08:24:11 -0300

    finished

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -46,7 +46,9 @@ export class HomeComponent {
   }
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
-    this.filteredLocationList = this.housingLocationList;
+    this.housingService.getAllHousingLocations().then((housingLocationList: HousingLocation[]) => {
+      this.housingLocationList = housingLocationList;
+      this.filteredLocationList = housingLocationList;
+    });
   }
 }
